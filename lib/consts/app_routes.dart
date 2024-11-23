7541cbdaae7ff7fba73d1be56cb88c5601db266f
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-11-23 16:51:02 +0700

    refact: UI flows and code base

--- lib/consts/app_routes.dart
+++ lib/consts/app_routes.dart
@@ -4,11 +4,14 @@ class AppRoutes {
   static const String homeChat = '/home-chat';
   static const String chatBotAI = '/chatbot-ai';
   static const String knowledgeBase = '/knowledge-base';
-  static const String register = '/authentication/register';
-  static const String loginGmail = '/authentication/login-gmail';
+
   static const String userProfile = '/profile';
   static const String kbDetails = '/knowledge-base/details';
   static const String promptPage = '/prompt';
   static const String updateAccount = '/update-account';
+
+  static const String register = '/authentication/register';
+  static const String loginGmail = '/authentication/login-gmail';
   static const String loginPage = '/authentication/login';
+  static const String resetPassword = '/authentication/reset-password';
 }
