4ce1f8f15296a8ea64fbef94b7c1d158636ea98a
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-12-06 13:27:08 +0700

    chore: config api key for the app

--- lib/consts/api.dart
+++ lib/consts/api.dart
@@ -1,2 +1,4 @@
-const devServer = 'https://api.dev.jarvis.cx';
+// const devServer = 'https://api.dev.jarvis.cx';
 const prodServer = 'https://api.jarvis.cx';
+const kbAPIUrl = 'https://knowledge-api.jarvis.cx';
+const devServer = 'https://api.jarvis.cx';
